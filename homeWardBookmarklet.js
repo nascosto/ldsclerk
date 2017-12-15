da316d96c9cfde5d31be897ea2b35e83df71fd8e
Benjamin Sellers <bsellers@gmail.com>
2017-12-14 19:58:11 -0700

    Cleanup

--- homeWardBookmarklet.js
+++ homeWardBookmarklet.js
@@ -1,69 +1,70 @@
-javascript:(function () { 
-    const memberListUrl = 'https://www.lds.org/mls/mbr/services/report/member-list?lang=eng';
-    const memberProfileUrl = 'https://www.lds.org/mls/mbr/records/member-profile/service/';
+javascript: (function () {
+	const memberListUrl = 'https://www.lds.org/mls/mbr/services/report/member-list?lang=eng';
+	const memberProfileUrl = 'https://www.lds.org/mls/mbr/records/member-profile/service/';
+	const unitDetailsUrl = 'https://www.lds.org/mls/mbr/services/cdol/details/unit/';
 
-    let csvMembersWithParents = 'Name,"Father\'s Name","Father\'s Unit","Mother\'s Name","Mother\'s Unit"\n';
+	let csvMembersWithParents = 'Name,"Father\'s Name","Father\'s Unit","Mother\'s Name","Mother\'s Unit"\n';
 
-    let memberProfileCounter;
+	let memberProfileCounter;
 
-    generateHomeWardList();
+	generateHomeWardList();
 
-    function generateHomeWardList() {
-        requestJson(memberListUrl, onGetMemberListSuccess, () => console.error('Failed to get member list.'));
-    }
+	function generateHomeWardList() {
+		requestJson(memberListUrl, onGetMemberListSuccess, () => console.error('Failed to get member list.'));
+	}
 
-    function onGetMemberListSuccess(list) {
-        memberProfileCounter = list.length;
-        for (let i = 0; i < list.length; i++) {
-            requestJson(memberProfileUrl + list[i].id + '?lang=eng', onGetMemberProfileSuccess, () => console.error('Failed to get member profile ' + list[i].id));
-        }
-    }
+	function onGetMemberListSuccess(list) {
+		memberProfileCounter = list.length;
+		for (let i = 0; i < list.length; i++) {
+			requestJson(`${memberProfileUrl}${list[i].id}?lang=eng`, onGetMemberProfileSuccess, () => console.error('Failed to get member profile ' + list[i].id));
+		}
+	}
 
-    function onGetMemberProfileSuccess(member) {
-        let name = member.individual.name;
-        let fatherName = member.family.parents && member.family.parents.father && member.family.parents.father.name ? member.family.parents.father.name : '';
-        let fatherUnit = member.family.parents && member.family.parents.father && member.family.parents.father.unitNumber ? member.family.parents.father.unitNumber : '';
-        let motherName = member.family.parents && member.family.parents.mother && member.family.parents.mother.name ? member.family.parents.mother.name : '';
-        let motherUnit = member.family.parents && member.family.parents.mother && member.family.parents.mother.unitNumber ? member.family.parents.mother.unitNumber : '';
-        csvMembersWithParents += '"' + name + '","' + fatherName + '",' + fatherUnit + ',"' + motherName + '",' + motherUnit + '\n';
+	function onGetMemberProfileSuccess(member) {
+		let name = member.individual.name;
+		let fatherName = member.family.parents && member.family.parents.father && member.family.parents.father.name ? member.family.parents.father.name : '';
+		let fatherUnit = member.family.parents && member.family.parents.father && member.family.parents.father.unitNumber ? member.family.parents.father.unitNumber : '';
+		let motherName = member.family.parents && member.family.parents.mother && member.family.parents.mother.name ? member.family.parents.mother.name : '';
+		let motherUnit = member.family.parents && member.family.parents.mother && member.family.parents.mother.unitNumber ? member.family.parents.mother.unitNumber : '';
+		csvMembersWithParents += `"${name}","${fatherName}",${fatherUnit},"${motherName}",${motherUnit}\n`;
 
-        memberProfileCounter--;
-        if (memberProfileCounter === 0) {
-            saveMembersWithParents(csvMembersWithParents);
-        }
-    }
+		memberProfileCounter--;
+		if (memberProfileCounter === 0) {
+			saveMembersWithParents(csvMembersWithParents);
+		}
+	}
 
-    function requestJson(url, onSuccess, onError) {
-        let xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    if (onSuccess) {
-                        let response = JSON.parse(xhr.responseText);
-                        onSuccess(response);
-                    }
-                } else {
-                    if (onError) {
-                        onError();
-                    }
-                }
-            }
-        };
-        xhr.open('GET', url);
-        xhr.send();
-    }
+	function requestJson(url, onSuccess, onError) {
+		let xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					if (onSuccess) {
+						let response = JSON.parse(xhr.responseText);
+						onSuccess(response);
+					}
+				} else {
+					if (onError) {
+						onError();
+					}
+				}
+			}
+		};
+		xhr.open('GET', url);
+		xhr.send();
+	}
 
-    function saveMembersWithParents(text) {
-        let blob = new Blob([text], {type: 'text/plain'});
+	function saveMembersWithParents(text) {
+		let blob = new Blob([text], { type: 'text/plain' });
 
-        var a = document.createElement('a');
-        a.style = 'display: none';
-        a.href = window.URL.createObjectURL(blob);
-        a.download = 'homeWard.csv';
+		var a = document.createElement('a');
+		a.style = 'display: none';
+		a.href = window.URL.createObjectURL(blob);
+		a.download = 'homeWard.csv';
 
-        document.body.appendChild(a);
-        a.click();
-        window.URL.revokeObjectURL(url);
-        document.body.removeChild(a);
-    }
+		document.body.appendChild(a);
+		a.click();
+		window.URL.revokeObjectURL(url);
+		document.body.removeChild(a);
+	}
 }());
--- newMemberEmailBookmarklet.js
+++ newMemberEmailBookmarklet.js
@@ -1,61 +1,61 @@
-javascript:(function () { 
-    const newMemberUrl = 'https://www.lds.org/mls/mbr/services/report/members-moved-in/unit/472050/3?lang=eng';
-    const memberProfileUrl = 'https://www.lds.org/mls/mbr/records/member-profile/service/';
+javascript: (function () {
+	const newMemberUrl = 'https://www.lds.org/mls/mbr/services/report/members-moved-in/unit/472050/3?lang=eng';
+	const memberProfileUrl = 'https://www.lds.org/mls/mbr/records/member-profile/service/';
 
-    let csvNewMembers = 'Name,"Move in Date","Individual Email","Household Email"\n';
+	let csvNewMembers = 'Name,"Move in Date","Individual Email","Household Email"\n';
 
-    generateNewMemberList();
+	generateNewMemberList();
 
-    function generateNewMemberList() {
-        requestJson(newMemberUrl, onGetNewMemberListSuccess, () => console.error('Failed to get new member list.'));
-    }
+	function generateNewMemberList() {
+		requestJson(newMemberUrl, onGetNewMemberListSuccess, () => console.error('Failed to get new member list.'));
+	}
 
-    function onGetNewMemberListSuccess(list) {
-        for (let member of list) {
-            processNewMember(member);
-        }
-        saveNewMembers(csvNewMembers);
-    }
+	function onGetNewMemberListSuccess(list) {
+		for (let member of list) {
+			processNewMember(member);
+		}
+		saveNewMembers(csvNewMembers);
+	}
 
-    function processNewMember(member) {
-        let name = member.spokenName;
-        let moveInDate = member.moveDate;
-        let individualEmail = member.email;
-        let householdEmail = member.householdEmail;
-        csvNewMembers += '"' + name + '",' + moveInDate + ',' + individualEmail + ',' + householdEmail + '\n';
-    }
+	function processNewMember(member) {
+		let name = member.spokenName;
+		let moveInDate = member.moveDate;
+		let individualEmail = member.email;
+		let householdEmail = member.householdEmail;
+		csvNewMembers += '"' + name + '",' + moveInDate + ',' + individualEmail + ',' + householdEmail + '\n';
+	}
 
-    function requestJson(url, onSuccess, onError) {
-        let xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    if (onSuccess) {
-                        let response = JSON.parse(xhr.responseText);
-                        onSuccess(response);
-                    }
-                } else {
-                    if (onError) {
-                        onError();
-                    }
-                }
-            }
-        };
-        xhr.open('GET', url);
-        xhr.send();
-    }
+	function requestJson(url, onSuccess, onError) {
+		let xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					if (onSuccess) {
+						let response = JSON.parse(xhr.responseText);
+						onSuccess(response);
+					}
+				} else {
+					if (onError) {
+						onError();
+					}
+				}
+			}
+		};
+		xhr.open('GET', url);
+		xhr.send();
+	}
 
-    function saveNewMembers(text) {
-        let blob = new Blob([text], {type: 'text/plain'});
+	function saveNewMembers(text) {
+		let blob = new Blob([text], { type: 'text/plain' });
 
-        var a = document.createElement('a');
-        a.style = 'display: none';
-        a.href = window.URL.createObjectURL(blob);
-        a.download = 'newMembers.csv';
+		var a = document.createElement('a');
+		a.style = 'display: none';
+		a.href = window.URL.createObjectURL(blob);
+		a.download = 'newMembers.csv';
 
-        document.body.appendChild(a);
-        a.click();
-        window.URL.revokeObjectURL(url);
-        document.body.removeChild(a);
-    }
+		document.body.appendChild(a);
+		a.click();
+		window.URL.revokeObjectURL(url);
+		document.body.removeChild(a);
+	}
 }());
